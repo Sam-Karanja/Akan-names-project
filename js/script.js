7a1143a1fdcb4f794cd78f5193dae2a28143f0c7
Moringa School <karanjasam210@gmail.com>
2022-03-07 00:14:01 +0300

    ci: declare variables dd, mm, year, yy and cc and parsed arguments

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 const weekday=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 console.log(weekday[3]);
+
+let data = document.getElementById("form")
+form.addEventListener('submit',function calculate(event) {
+    event.preventDefault();
+
+    let dd = parseInt(document.getElementById('date').value);
+    if (dd < 0 || dd > 31){
+        alert("You have entered an invalid date")
+    }
+    
+    let mm = parseInt(document.getElementById('month').value);
+    if(mm < 0 || mm > 12){
+        alert("You have entered an invalid month")
+    }
+
+    let year = parseInt(document.getElementById('year').value);
+    let cc = parseInt(document.getElementById('year').value.slice(0,2));
+    console.log(cc)
+    let yy = parseInt(document.getElementById('year').value.slice(2,4));
+    console.log(yy);
+
+})
