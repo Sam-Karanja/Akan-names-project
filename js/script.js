75c6783e3ff199e17ec3f3d985ccb00026c72971
Moringa School <karanjasam210@gmail.com>
2022-03-07 01:01:39 +0300

    ci: add function to calculate the day of the week

--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
-const weekday=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
-console.log(weekday[3]);
+const days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+console.log(days[3]);
 
 let data = document.getElementById("form")
 form.addEventListener('submit',function calculate(event) {
     event.preventDefault();
 
-    let dd = parseInt(document.getElementById('date').value);
-    if (dd < 0 || dd > 31){
+    let DD = parseInt(document.getElementById('date').value);
+    if (DD < 0 || DD > 31){
         alert("You have entered an invalid date")
     }
     
-    let mm = parseInt(document.getElementById('month').value);
-    if(mm < 0 || mm > 12){
+    let MM = parseInt(document.getElementById('month').value);
+    if(MM < 0 || MM > 12){
         alert("You have entered an invalid month")
     }
 
     let year = parseInt(document.getElementById('year').value);
-    let cc = parseInt(document.getElementById('year').value.slice(0,2));
-    console.log(cc)
-    let yy = parseInt(document.getElementById('year').value.slice(2,4));
-    console.log(yy);
+    let CC = parseInt(document.getElementById('year').value.slice(0,2));
+    console.log(CC)
+    let YY = parseInt(document.getElementById('year').value.slice(2,4));
+    console.log(YY);
+
+    let gender = document.getElementById('gender').value;
+    console.log(gender) 
+
+    let dayOfTheWeek =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+    document.getElementById(weekday).value = days[dayOfTheWeek];
 
 })
