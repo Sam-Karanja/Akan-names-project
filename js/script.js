2e5901bcc077988bb82fcf0964126486ffd555a0
Moringa School <karanjasam210@gmail.com>
2022-03-07 01:46:40 +0300

    test: removed results section to test with alert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-const days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-console.log(days[3]);
+const days=[];
+days[0] = "Sunday"
+days[1] = "Monday"
+days[2] = "Tuesday"
+days[3] = "Wednesday"
+days[4] = "Thursday"
+days[5] = "Friday"
+days[6] = "Saturday"
+console.log(days[5]);
 
 let data = document.getElementById("form")
 form.addEventListener('submit',function calculate(event) {
@@ -24,56 +31,61 @@ form.addEventListener('submit',function calculate(event) {
     let gender = document.getElementById('gender').value;
     console.log(gender) 
 
-    let dayOfTheWeek = Math.trunc((((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7)
-    document.getElementById(weekday).value = days[dayOfTheWeek];
+    let dayOfTheWeek = Math.trunc((((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7)   
+    console.log(dayOfTheWeek)
+
+    document.getElementById('weekday').value = 'days[dayOfTheWeek]';
 
     if(gender === male){
         if(dayOfTheWeek === 0){
-            document.getElementById('akan').value = "Kwasi"
+           alert("Kwasi")
         }
         else if(dayOfTheWeek === 1){
-            document.getElementById('akan').value = "Kwadwo"
+            alert("Kwadwo")
         }
         else if(dayOfTheWeek === 2){
-            document.getElementById('akan').value = "Kwabena"
+            alert("Kwabena")
         }
         else if(dayOfTheWeek === 3){
-            document.getElementById('akan').value = "Kwaku"
-        }
+            alert("Kwaku")
+        } 
         else if(dayOfTheWeek === 4){
-            document.getElementById('akan').value = "Yaw"
+        alert("Yaw")
         }
+
         else if(dayOfTheWeek === 5){
-            document.getElementById('akan').value = "Kofi"
+          alert("Kofi")
         }
         else if(dayOfTheWeek === 6){
-            document.getElementById('akan').value = "Kwame"
+           alert("Kwame")
         }
 
 }else if(gender === female){
     if(dayOfTheWeek === 0){
-        document.getElementById('akan').value = "Akosua"
+        document.getElementById('akan').value = "Akosua";
     }
     else if(dayOfTheWeek === 1){
-        document.getElementById('akan').value = "Adwoa"
+        document.getElementById('akan').value = "Adwoa";
     }
     else if(dayOfTheWeek === 2){
-        document.getElementById('akan').value = "Abenaa"
+        document.getElementById('akan').value = "Abenaa";
     }
     else if(dayOfTheWeek === 3){
-        document.getElementById('akan').value = "Akua"
+        document.getElementById('akan').value = "Akua";
     }
     else if(dayOfTheWeek === 4){
-        document.getElementById('akan').value = "Yaa"
+        document.getElementById('akan').value = "Yaa";
     }
     else if(dayOfTheWeek === 5){
-        document.getElementById('akan').value = "Afua"
+        document.getElementById('akan').value = "Afua";
     }
-    else if(dayOfTheWeek === 6){
-        document.getElementById('akan').value = "Ama"
+    else{
+        document.getElementById('akan').value = "Ama";
     }
 
 }
     
 
-})
+});
+
+
